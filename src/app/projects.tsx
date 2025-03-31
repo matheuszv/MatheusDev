0cbbe5aeab2be1fffa46c3cfad0b0238b9b80858
Matheus Vinagre <79124620+matheuszv@users.noreply.github.com>
2025-03-31 19:44:08 -0300

    adaptation mobile

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -13,26 +13,26 @@ export function Projects(){
         target: targetRef,
       });
 
-      const x = useTransform(scrollYProgress, [0, 1], ["10%", "-50%"]);
+      const x = useTransform(scrollYProgress, [0, 1], ["5%", "-55%"]);
     return (
-        <section ref={targetRef} className="relative h-[300vh] bg-[url('./bg-desktop4.jpg')] bg-cover md:h-[400vh]">
+        <section ref={targetRef} className="relative h-[300vh] bg-[url('./bg-desktop4.jpg')] bg-cover">
         <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-            <motion.div style={{ x }} className="flex gap-8 px-6">
-                <a href="https://lembre-lembre-app.vercel.app/fotos/" className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[450px]  max-md:max-w-[330px]">
+            <motion.div style={{ x }} className="flex gap-8 px-6 max-md:gap-2">
+                <a href="https://lembre-lembre-app.vercel.app/fotos/" className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[500px]  max-md:max-w-[260px]">
                     <Image src={LembreImage.src} alt="Projeto de Matheus" className="object-cover" width={450} height={250} preview={false}/>
                     <div className="p-4 max-w-[450px]">
                     <h3 className="text-xl font-semibold text-cyan-300">Lembre-Lembre</h3>
                     <p className="text-gray-400 text-sm mt-2">Complete photo and event management and organization system, developed with Next.js, Prisma, and Tailwind CSS.</p>
                     </div>
                 </a>
-                <a href="https://baliarquitetura.com/" className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[450px] max-md:max-w-[330px]">
+                <a href="https://baliarquitetura.com/" className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[500px] max-md:max-w-[260px]">
                     <Image src={BaliImage.src} alt="Projeto de Matheus" className="object-cover" width={450} height={250} preview={false}/>
                     <div className="p-4 max-w-[450px]">
                     <h3 className="text-xl font-semibold text-cyan-300">BALI ARQUITETURA</h3>
                     <p className="text-gray-400 text-sm mt-2">Platform that displays information and projects carried out by BALI Architecture, a project using Next.js.</p>
                     </div>
                 </a>
-                <a href="https://dt-money-mv.vercel.app/" className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[450px]  max-md:max-w-[330px]">
+                <a href="https://dt-money-mv.vercel.app/" className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[500px]  max-md:max-w-[260px]">
                     <Image src={DTimage.src} alt="Projeto de Matheus" className="object-cover" width={450} height={250} preview={false}/>
                     <div className="p-4 max-w-[450px]">
                     <h3 className="text-xl font-semibold text-cyan-300">DT-Money</h3>
@@ -40,7 +40,7 @@ export function Projects(){
                     </div>
                 </a>
                 {/* <a className="flex flex-col bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 max-w-[450px]">
-                    <Image src={matheusfoto.src} alt="Projeto de Matheus" className="object-cover" width={450} height={250} preview={false}/>
+                    <Image src={DTimage.src} alt="Projeto de Matheus" className="object-cover" width={450} height={250} preview={false}/>
                     <div className="p-4 max-w-[450px]">
                     <h3 className="text-xl font-semibold text-cyan-300">Lembre-Lembre</h3>
                     <p className="text-gray-400 text-sm mt-2">Sistema completo de gestão e organização de fotos e eventos, desenvolvido com Next.js, Prisma e Tailwind CSS.</p>
