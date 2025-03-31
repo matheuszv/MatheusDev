386cfcab9b909dc0cc19a6b474eb5c05671f0e2b
Matheus Vinagre <79124620+matheuszv@users.noreply.github.com>
2025-03-31 17:55:04 -0300

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
+import ico from './pngegg.ico'
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Matheus Portfólio",
   description: "Portfólio de Matheus Vinagre – Desenvolvedor Full-Stack. Especializado em criar experiências digitais com tecnologias modernas, este site reúne meus projetos, habilidades e soluções para aplicações web eficientes e escaláveis.",
+  icons: ico.src,
 };
 
 
