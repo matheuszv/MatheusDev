bbd7d676a34b289a0c10d02b91525a0ebbde1835
Matheus Vinagre <79124620+matheuszv@users.noreply.github.com>
2025-03-31 12:51:00 -0300

    alerta funcioinando

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -1,13 +1,20 @@
 'use client'
 import { Button } from "@/components/ui/button";
 import { MessageCirclePlus } from "lucide-react";
+import { Alert } from "antd";
+import { useState } from "react";
 
 
 export function Contact(){
+    const [alert, setAlert] = useState(false)
     const phoneNumber = "5583998594656"; // Seu número com DDD e DDI sem espaços ou símbolos
     const message = "Hi, I would like to know more about your service."; // Mensagem pronta
     const encodedMessage = encodeURIComponent(message);
     const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    function sleep(ms: number): Promise<void> {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleSubmit = async (e: any) => {
@@ -20,9 +27,12 @@ export function Contact(){
           });
       
           if (response.ok) {
-            alert("E-mail enviado com sucesso!");
+            setAlert(true)
+            e.target.reset();
+            await sleep(2000)
+            setAlert(false)
           } else {
-            alert("Erro ao enviar e-mail.");
+            console.log("Erro ao enviar e-mail:");
           }
         } catch (error) {
           console.error("Erro ao enviar e-mail:", error);
@@ -32,6 +42,13 @@ export function Contact(){
 
     return(
         <div className="flex flex-col items-center justify-center min-h-screen bg-[#121214] p-4">
+          {alert && <Alert
+            className="fixed top-16 right-3"
+            message="Success"
+            description="The email was sent successfully!"
+            type="success"
+            showIcon
+          />}
         <div className="w-full max-w-xl p-6 rounded-lg shadow-lg">
           <h2 className="text-3xl font-bold text-white mb-4">Contact.</h2>
           <p className="text-[#b3b3b3] mb-6">
